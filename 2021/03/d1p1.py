4881692bfd28c75426a030888f1fab79c660bec0
Dylan-Patel <68872212+Dylan-Patel@users.noreply.github.com>
2021-12-04 00:42:15 +0000

    Delete d1p1.py

--- 2021/03/d1p1.py
+++
@@ -1,9 +0,0 @@
-content = open("input.txt", "r")
-input = [int(i) for i in content.read().split("\n")]
-
-counter = 0
-for a in range((len(input) - 1)):
-      if input[a+1] > input [a]:
-      	counter += 1
-
-print(counter)
